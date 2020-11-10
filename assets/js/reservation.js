3349222ff8e5e1c8d3261157887105368125d82d
cesargtz <cesareomargtz@gmail.com>
2020-11-09 19:15:14 -0700

    version 0.1.0

---
+++ assets/js/reservation.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("book_today").addEventListener("click",() => {
+        // $("#modal").iziModal({
+        //     headerColor: '#77b748',
+        //     fullscreen: true,
+        //     width: 1000,
+        //     height: 600,
+        //     transitionIn: 'fadeInDown',
+        //     transitionOut: 'fadeOutDown',
+        //   });
+    
+        // var checkIn = document.getElementById("check-in").innerHTML
+        // var checkOut = document.getElementById("check-out").innerHTML
+        // var personTotal = document.getElementById("person-total").innerHTML
+        // number = "XXXXXXXXXX"
+        // message = `https://wa.me/${number}?text=Reservacion%20`
+    })
+});
